d01a092a9fec6ca196fed5a09bbdcbe51536a962
isoolio110 <kim.isul@gmail.com>
2015-05-28 19:20:33 -0400

    Adds reference to Posts model

--- reddit-clone/app.js
+++ reddit-clone/app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/news');
 
+require('./models/Posts')
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
