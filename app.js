632066ff49393b37a355dd05160d3be5721be8ca
isoolio110 <kim.isul@gmail.com>
2015-05-28 17:31:11 -0400

    Injects posts to MainCtrl

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-var app = angular.module('RedditClone', []);
+var app = angular.module('RedditClone', [])
 .factory('posts', [function(){
   var o = {
-    posts: []
+    posts: [{title: 'hello', link:'', upvotes: 0}]
   };
   return o;
-}]);
+}])
 
 .controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
   $scope.test = 'Hello world!';
-
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ]
-
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
     if ($scope.title && $scope.title.length > 0) {
       $scope.posts.push({
