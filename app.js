6e6e68981c154b2a88397604e08108a1123e544e
isoolio110 <kim.isul@gmail.com>
2015-05-28 15:52:09 -0400

    Adds function to add a post

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.test = 'Hello world!';
+
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -11,4 +12,9 @@ function($scope){
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
   ]
+
+  $scope.addPost = function(){
+    $scope.posts.push({title: "A new post", upvotes: 4});
+  }
+
 }]);
