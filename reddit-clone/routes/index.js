461fd0a378c411e38ebd2e8a212c3a1345ca95a0
isoolio110 <kim.isul@gmail.com>
2015-05-28 20:17:20 -0400

    Adds put route for upvoting a post

--- reddit-clone/routes/index.js
+++ reddit-clone/routes/index.js
@@ -34,11 +34,20 @@ router.param('post', function(req, res,
     if (!post) {return next(new Error("can't find post"));}
     req.post = post;
     return next();
-  })
-})
+  });
+});
 
 router.get('/posts/:post', function(req, res){
   res.json(req.post);
-})
+});
+
+router.put('/posts/:post/upvote', function(req,res,next){
+  req.post.upvote(function(err,post){
+    if (err) {return next(err);}
+    res.json(post)
+  });
+});
+
+
 
 module.exports = router;
